eb585f603f022c0d674ec5b9a58c696ddc42a62c
MattCarl_Scheffel <mcs9ff@virginia.edu>
2022-10-30 18:54:19 -0400

    setup.py

---
+++ M09/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='M09',
+      version='0.1',
+      description='M09 HW Package',
+      url='https://github.com/mcs9ff/M09Repo/tree/main/M09',
+      author='Matt Scheffel',
+      author_email='mcs9ff@virginia.edu',
+      license='GNU',
+      packages=['M09'])
